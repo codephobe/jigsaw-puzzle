92615d8bf6f24d7f2a96ca9bae9e48d5c825b627
Jake Tom <srsjake@gmail.com>
2017-05-23 10:30:41 +0300

    code style

--- js/difficulty.js
+++ js/difficulty.js
@@ -1,30 +1,30 @@
 (function() {
-	//Cache DOM
-	var timerBtn = document.getElementById('timerBtn');
-	var hintBtn = document.getElementById('hintBtn');
-	var audioButton = document.getElementById('audio-button');
+  //Cache DOM
+  var timerBtn = document.getElementById('timerBtn');
+  var hintBtn = document.getElementById('hintBtn');
+  var audioButton = document.getElementById('audio-button');
 
-	events.on('difficulty', handleDifficulty);
+  events.on('difficulty', handleDifficulty);
 
-	initTimer();
-	initHints();
+  initTimer();
+  initHints();
 
-	function handleDifficulty(difficulty) {
-		audioButton.play();
-		initTimer(difficulty);
-		initHints(difficulty);
-	}
+  function handleDifficulty(difficulty) {
+    audioButton.play();
+    initTimer(difficulty);
+    initHints(difficulty);
+  }
 
-	function initTimer(difficulty) {
-		if(difficulty) {
-			timerBtn.innerHTML = '5:00';
-		} else {
-			timerBtn.innerHTML = '8:00';
-		}
-	}
+  function initTimer(difficulty) {
+    if(difficulty) {
+      timerBtn.innerHTML = '5:00';
+    } else {
+      timerBtn.innerHTML = '8:00';
+    }
+  }
 
-	function initHints(difficulty) {
-		var hints = difficulty ? 3 : 6;
-		hintBtn.innerHTML = 'HINTS: ' + hints;
-	}
+  function initHints(difficulty) {
+    var hints = difficulty ? 3 : 6;
+    hintBtn.innerHTML = 'HINTS: ' + hints;
+  }
 })();
